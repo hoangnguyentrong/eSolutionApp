11c1a2b3a44e676eb92a4a39f3bb67cad72ecf35
Nguyễn Trọng Hoàng <hoangnguyen30092003@gmail.com>
2024-08-03 16:31:58 +0700

    -update

--- eSolutionApp/obj/Debug/net8.0/eSulutionApp.AssemblyInfo.cs
+++ eSolutionApp/obj/Debug/net8.0/eSulutionApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSulutionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889e1bed8cfebf101d7c69ee52e5859f4fa6c8bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSulutionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSulutionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
